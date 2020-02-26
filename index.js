63fff521ee3ad155dbd8d60c2ad3e6f59000608c
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-02-26 14:03:05 -0800

    deleted previous code to start fresh

--- index.js
+++ index.js
@@ -40,26 +40,7 @@ class Airplane {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-class Person {
-  
-  constructor(name, age, attributes) {
-    this.stomach = [];
-    this.name = name,
-    this.age = age
-    
-  } 
-  eat(edible) {
-    if (this.stomach.length < 10) {
-      this.stomach.push(edible)
-    };
-  } 
-  poop() {
-    this.stomach = [];
-  } 
-  toString() {
-    return `${this.name}, ${this.age}`;
-  }
-}
+
 
 /*
   TASK 2
@@ -75,29 +56,7 @@ class Person {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-class Car {
-  constructor(model, milesPerGallon) {
-    this.tank = 0,
-    this.odometer = 0,
-    this.model = model,
-    this.milesPerGallon = milesPerGallon
-  } 
-  fill(gallons) {
-    return (this.tank = this.tank + gallons);
-  }
-  drive(distance) {
-    let fuelUsed = distance / this.milesPerGallon;
-    let milesAvailable = this.tank * this.milesPerGallon;
-    if (this.tank >= fuelUsed) {
-    this.odometer += distance,
-    this.tank -= fuelUsed;
-  } else {
-    this.odometer += milesAvailable;
-    this.tank = 0;
-    return `I ran out of fuel at ${this.odometer} miles!`;
-  }
-  }
-}
+
 
 /*
   TASK 3
@@ -111,16 +70,6 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
-class Lambdasian {
-  constructor(stats){
-      this.name = stats.name,
-      this.age = stats.age,
-      this.location = stats.location
-    }// write methods here
-    speak(){
-      return `Hello my name is ${this.name} I am from ${this.location}`;
-    }
-  }// this closes the parent
 
 /*
   TASK 4
@@ -136,20 +85,6 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor extends Lambdasian{
-  constructor(childStats) {
-    super(childStats),
-    this.specialty = childStats.specialty,
-    this.favLanguage = childStats.favLanguage,
-    this.catchPhrase = childStats.catchPhrase
-  } 
-  demo(subject) {
-    return `Today we are learning about ${subject}`;
-  }
-  grade(student, subject) {
-    return `${student.name} receives a perfect score on ${subject}`;
-  }
-}
 
 /*
   TASK 5
@@ -166,23 +101,7 @@ class Instructor extends Lambdasian{
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student extends Lambdasian {
-  constructor(studentStats) {
-    super(studentStats),
-    this.previousBackground = studentStats.previousBackground,
-    this.className = studentStats.className,
-    this.favSubjects = studentStats.favSubjects
-  } 
-  listSubjects() {
-   return `Loving ${this.favSubjects}`;
-  }
-  PRAssignment(subject) {
-    return `${this.name} has submitted a PR for ${subject}`;
-  }
-  sprintChallenge(subject) {
-    return `${this.name} has begun sprint challenge on ${subject}`;
-  }
-}
+c
 
 /*
   TASK 6
@@ -197,19 +116,7 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager extends Instructor {
-  constructor(manStats) {
-    super(manStats),
-    this.gradClassName = manStats.gradClassName,
-    this.favInstructor = manStats.favInstructor    
-    } 
-    standUp(channel) {
-      return `${this.name} announces to ${channel}, standy times!`;
-    }
-    debugsCode (student, subject) {
-      return `${this.name} debugs ${student.name}'s code on ${subject}`;
-    }
-}
+
 
 
 /*
